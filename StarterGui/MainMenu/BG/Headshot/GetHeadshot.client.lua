80f1077f277809a116726a08a4ff1698b9d25cbf
Byte <32001480+PcTechery@users.noreply.github.com>
2022-03-12 23:13:49 -0500

    Delete StarterGui directory

--- StarterGui/MainMenu/BG/Headshot/GetHeadshot.client.lua
+++
@@ -1,3 +0,0 @@
-local Player = game.Players.LocalPlayer
-local content, isready = game.Players:GetUserThumbnailAsync(Player.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size100x100)
-script.Parent.Image = content
